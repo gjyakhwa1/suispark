3997ba029fd93613194f4ff1a6002e88606a09d2
gjyakhwa1 <gjyakhwa1@gmail.com>
2025-02-06 19:28:35 +0545

    sidebar ui update

--- suispark-frontend/src/components/Sidebar/index.tsx
+++ suispark-frontend/src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu, Plus, LogOut } from "lucide-react";
+import { Menu, LogOut, SquarePen } from "lucide-react";
 import { useLogin } from "../../context/UserContext";
 import IRoom from "../../types/room.interface";
 
@@ -9,6 +9,7 @@ interface SidebarProps {
   onNewRoom: () => void;
   rooms: IRoom[];
   setActiveRoom: (activeRoom: string) => void;
+  activeRoom: string | null;
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({
@@ -16,23 +17,24 @@ export const Sidebar: React.FC<SidebarProps> = ({
   onToggle,
   onNewRoom,
   rooms,
+  activeRoom,
   setActiveRoom,
 }) => {
   const { logOut } = useLogin();
-
+  console.log(activeRoom);
   return (
     <>
       {!isOpen && (
         <button
           onClick={onToggle}
-          className="fixed top-4 left-4 p-2  text-black rounded hover:bg-gray-100 z-30"
+          className="fixed top-4 left-4 z-30 flex items-center p-2 rounded-lg hover:bg-gray-200"
         >
-          <Menu size={20} />
+          <Menu size={20} className="text-[#5d5d5d]"/>
         </button>
       )}
 
       <div
-        className={`fixed top-0 left-0 h-full bg-gray-900 text-white transition-all duration-300 ${
+        className={`fixed top-0 left-0 h-full bg-[#f9f9f9] text-white transition-all duration-300 ${
           isOpen ? "w-64" : "w-0 overflow-hidden"
         } z-20`}
       >
@@ -41,17 +43,21 @@ export const Sidebar: React.FC<SidebarProps> = ({
             <div className="flex items-center justify-between p-4">
               <button
                 onClick={onToggle}
-                className="p-2 hover:bg-gray-800 rounded"
+                className="flex items-center p-2 rounded-lg hover:bg-gray-200"
               >
-                <Menu size={20} />
+                <Menu size={20} className="text-[#5d5d5d]" />
               </button>
-              <button
-                onClick={onNewRoom}
-                className="flex items-center space-x-2 px-3 py-2 bg-blue-600 rounded hover:bg-blue-700"
-              >
-                <Plus size={16} />
-                <span>New Room</span>
-              </button>
+              <div className="relative group">
+                <button
+                  onClick={onNewRoom}
+                  className="flex items-center p-2 rounded-lg hover:bg-gray-200"
+                >
+                  <SquarePen size={20} className="text-[#5d5d5d]" />
+                </button>
+                <span className="absolute z-50 top-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-xs text-white bg-gray-800 rounded opacity-0 transition-opacity group-hover:opacity-100">
+                  Submit Your Idea
+                </span>
+              </div>
             </div>
 
             <nav className="mt-4 overflow-y-auto h-[calc(100vh-100px)]">
@@ -59,20 +65,27 @@ export const Sidebar: React.FC<SidebarProps> = ({
                 <button
                   key={room.id}
                   onClick={() => setActiveRoom(room.id)}
-                  className={`w-full text-left p-4 hover:bg-gray-800 flex items-center space-x-2 `}
+                  className={"w-full text-left flex items-center"}
                 >
-                  <div className="truncate flex-1">{room.id}</div>
+                  <div
+                    className={`truncate flex-1 text-black hover:bg-gray-200 p-2 mx-2 rounded-lg ${
+                      activeRoom && activeRoom === room.id ? "bg-gray-300" : ""
+                    }`}
+                  >
+                    {room.id}
+                  </div>
                 </button>
               ))}
             </nav>
-
-            <button
-              onClick={logOut}
-              className="absolute bottom-4 left-0 w-full p-4 hover:bg-gray-800 flex items-center justify-center space-x-2"
-            >
-              <LogOut size={20} />
-              <span>Logout</span>
-            </button>
+            <div>
+              <button
+                onClick={logOut}
+                className="absolute bottom-4 left-0  w-[calc(100%-16px)] mx-2 p-2 hover:bg-gray-200 flex items-center gap-2 rounded-lg text-black"
+              >
+                <LogOut size={20} className="text-[#5d5d5d]" />
+                <span>Logout</span>
+              </button>
+            </div>
           </>
         )}
       </div>

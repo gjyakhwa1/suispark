ff86da33bef22b353b539dfb6250e2039ec38585
gjyakhwa1 <gjyakhwa1@gmail.com>
2025-02-18 14:09:51 +0545

    fe port in environment variable

--- suispark-frontend/vite.config.ts
+++ suispark-frontend/vite.config.ts
@@ -1,16 +1,19 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT) || 5000,
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
